4ce7891a641f0adc69e17f8314949c6d8347332d
Kenna Wylie <kennajae@crc-dot1x-nat-10-239-241-158.bu.edu>
2024-02-01 18:59:26 -0500

    maybe works???? included tests

--- assigns/assign01/assign01_03.ml
+++ assigns/assign01/assign01_03.ml
@@ -24,4 +24,15 @@
  *)
 
 let fast_fib (n : int) : int =
-  assert false (* REMOVE THIS LINE AND FILL IN YOUR SOLUTION *)
+  let rec fibo n x y : int =
+    if n = 0 || n = 1 
+      then x 
+      else fibo n (x + y) x
+    in fibo n 1 1
+
+    let _ = assert (fast_fib 0 = 1)
+    let _ = assert (fast_fib 1 = 1)
+    let _ = assert (fast_fib 2 = 2)
+    let _ = assert (fast_fib 3 = 3)
+    let _ = assert (fast_fib 4 = 5)
+    let _ = assert (fast_fib 5 = 8)
