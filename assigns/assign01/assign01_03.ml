0af7dc7ceb25b4fa1d59360bcb2bc595d01a351a
Kenna Wylie <kennajae@crc-dot1x-nat-10-239-241-158.bu.edu>
2024-02-01 19:59:10 -0500

    pls pls pls

--- assigns/assign01/assign01_03.ml
+++ assigns/assign01/assign01_03.ml
@@ -27,7 +27,7 @@ let fast_fib (n : int) : int =
   let rec fibo n x y : int =
     if n = 0 || n = 1 
       then 1
-      else fibo n (x + y) x
+      else fibo n y (x + y)
     in fibo n 1 1
 
 let _ = print_int (fast_fib 2)
