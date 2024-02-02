cd93c95d0135fe9eb051226856f59276228089b9
Kenna Wylie <kennajae@crc-dot1x-nat-10-239-241-158.bu.edu>
2024-02-01 23:42:09 -0500

    test

--- assigns/assign01/assign01_05.ml
+++ assigns/assign01/assign01_05.ml
@@ -52,7 +52,7 @@ let block_text (s : string) (min_width : int) (max_width : int) : string =
     else 
       let start : string = (String.sub s 0 wid) in
       start ^ String.make 1 '\n' ^ block (String.sub s (wid) (String.length s - wid)) wid
-  in block s wid
+  in block s 2
 
   (*let _ = print_string (block_text "ABCDEFGHIJ" 0 3)*)
   let _ = print_string (block_text "ABCDEFGHIJ" 2 3)
