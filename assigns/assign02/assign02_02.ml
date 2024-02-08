f6d4dd2c450704e70325c6a9ab68056eb9246440
Kenna Wylie <kennajae@crc-dot1x-nat-10-239-241-158.bu.edu>
2024-02-08 14:46:07 -0500

    added tests

--- assigns/assign02/assign02_02.ml
+++ assigns/assign02/assign02_02.ml
@@ -39,3 +39,9 @@ let recs_by_ingrs (l : recipe list) (s : ingr list) : recipe list =
       else
         find_rec xs
   in find_rec l
+
+let r1 = { name = "1" ; ingrs = ["a"; "b"; "d"] }
+let r2 = { name = "2" ; ingrs = ["a"; "c"; "e"] }
+let r3 = { name = "3" ; ingrs = ["b"; "c"] }
+let _ = assert (recs_by_ingrs [r1;r2;r3] ["a";"b";"c";"d"] = [r1;r3])
+let _ = assert (recs_by_ingrs [r1;r2;r3] ["a";"b";"c";"e"] = [r2;r3])
