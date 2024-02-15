5b687e86bd0a9e73bea511237bdfd83ff46860cb
Kenna Wylie <kennajae@crc-dot1x-nat-10-239-241-158.bu.edu>
2024-02-14 20:03:29 -0500

    success! removed test cases and added comments

--- assigns/assign03/assign03_01.ml
+++ assigns/assign03/assign03_01.ml
@@ -27,23 +27,23 @@ type 'a concatlist
 
 let mergesort l1 l2 =
   let rec merge a b =
+(* implemented mergesort function for concat *)
     match a, b with
+(* base cases where either a or b are empty *)
     | _, [] -> a
     | [], _ -> b
+(* recursive case where mergesort is performed *)
     | a1 :: a2, b1 :: b2 ->
       if a1 < b1
         then a1 :: merge a2 (b1 :: b2)
       else b1 :: merge b2 (a1 :: a2)
-
-    (*assert false merge sort function to be implemented (traditional merge sort)*)
   in merge l1 l2
 let sort (l : 'a concatlist) : 'a list =
   let rec sorting (lst : 'a concatlist) : 'a list =
+(* pattern matching type, only performing sorting if it's concat *)
     match lst with
     | Nil -> []
     | Single x -> [x]
-    | Concat(x, xs) -> mergesort (sorting x) (sorting xs) (*split x and xs and perform merge sort on each; IMPLEMENT LATER*)
+(*split x and xs and perform sort on each and merge sorting both lists*)
+    | Concat (x, xs) -> mergesort (sorting x) (sorting xs)
   in sorting l
-
-  let l = Concat (Concat (Single 3, Single 2), Concat (Single 1, Single 10))
-  let _ = assert (sort l = [1;2;3;10])
