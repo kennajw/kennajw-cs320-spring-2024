51a16ded8357ecc48b7dadeeddc24b69800b3680
Kenna Wylie <kennajae@crc-dot1x-nat-10-239-224-249.bu.edu>
2024-03-27 21:10:15 -0400

    should work now ahhhh

--- assigns/assign07/assign07.ml
+++ assigns/assign07/assign07.ml
@@ -130,7 +130,7 @@ let next_token (cs : char list) : (token * char list) option =
     | x :: xs when is_lower_case x ->
       if check_list cs
         then 
-          (let term, rest = span is_lower_case xs in
+          (let term, rest = span is_lower_case cs in
           Some (TmT (implode term), rest))
       else None
     | x :: xs when is_blank x -> next xs
